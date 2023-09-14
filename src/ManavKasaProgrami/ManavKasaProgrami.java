978e02ee5c25f9cab20c1cba01ea81987655ab2a
baris.karapelit <baris.karapelit@viennalife.com.tr>
2023-09-14 19:40:18 +0300

    Manav Kasa Programi

---
+++ src/ManavKasaProgrami/ManavKasaProgrami.java
@@ -0,0 +1,54 @@
+package ManavKasaProgrami;
+
+public class ManavKasaProgrami {
+    private static Double ArmutKg;
+    private static Double ElmaKg;
+    private static Double DomatesKg;
+    private static Double MuzKg;
+    private static Double PatlicanKg;
+
+    public Double getArmutKg() {
+        return ArmutKg;
+    }
+
+    public Double getElmaKg() {
+        return ElmaKg;
+    }
+
+    public Double getDomatesKg() {
+        return DomatesKg;
+    }
+
+    public Double getMuzKg() {
+        return MuzKg;
+    }
+
+    public Double getPatlicanKg() {
+        return PatlicanKg;
+    }
+
+    public void setArmutKg(Double armutKg) {
+        ArmutKg = armutKg;
+    }
+
+    public void setElmaKg(Double elmaKg) {
+        ElmaKg = elmaKg;
+    }
+
+    public void setDomatesKg(Double domatesKg) {
+        DomatesKg = domatesKg;
+    }
+
+    public void setMuzKg(Double muzKg) {
+        MuzKg = muzKg;
+    }
+
+    public void setPatlicanKg(Double patlicanKg) {
+        PatlicanKg = patlicanKg;
+    }
+
+
+    public Double hesapla(){
+        return (ArmutKg*2.14)+(ElmaKg*3.67)+(DomatesKg*1.11)+(MuzKg*0.95)+(PatlicanKg*5.00);
+    }
+}
--- src/PatikaPlus.java
+++ src/PatikaPlus.java
@@ -1,7 +1,34 @@
+import ManavKasaProgrami.ManavKasaProgrami;
 import VucutKitleEndeksiHesabi.VucutKitleEndeksiHesabi;
+
+import java.util.Scanner;
 
 public class PatikaPlus {
     public static void main(String[] args) {
+        //VucutKitleEndeksiHesabi();
+        ManavKasaProgrami();
+
+
+    }
+
+    private static void ManavKasaProgrami() {
+        ManavKasaProgrami manavKasaProgrami = new ManavKasaProgrami();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Mana Kasa Programı");
+        System.out.println("Armut Kaç Kilo ? : ");
+        manavKasaProgrami.setArmutKg(input.nextDouble());
+        System.out.println("Elma Kaç Kilo ? : ");
+        manavKasaProgrami.setElmaKg(input.nextDouble());
+        System.out.println("Domates Kaç Kilo ? : ");
+        manavKasaProgrami.setDomatesKg(input.nextDouble());
+        System.out.println("Muz Kaç Kilo ? : ");
+        manavKasaProgrami.setMuzKg(input.nextDouble());
+        System.out.println("Patlıcan Kaç Kilo ? : ");
+        manavKasaProgrami.setPatlicanKg(input.nextDouble());
+        System.out.println("Toplam Tutar : " + manavKasaProgrami.hesapla());
+    }
+
+    private static void VucutKitleEndeksiHesabi() {
         VucutKitleEndeksiHesabi hesap = new VucutKitleEndeksiHesabi();
         hesap.SetBoy(1.80);
         hesap.SetVucutKilogrami(80.0);
