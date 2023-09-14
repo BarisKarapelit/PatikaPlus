a1ff09eb4e3974c9261a6a48654483f253b62bc7
baris.karapelit <baris.karapelit@viennalife.com.tr>
2023-09-14 19:44:50 +0300

    commit added

--- src/ManavKasaProgrami/ManavKasaProgrami.java
+++ src/ManavKasaProgrami/ManavKasaProgrami.java
@@ -1,6 +1,7 @@
 package ManavKasaProgrami;
 
 public class ManavKasaProgrami {
+    //Urunlerin kilolari tanımlıyoruz.
     private static Double ArmutKg;
     private static Double ElmaKg;
     private static Double DomatesKg;
@@ -49,6 +50,7 @@ public class ManavKasaProgrami {
 
 
     public Double hesapla(){
+        //Tum urunlerin kilo fiyatlarını toplayıp donduruyoruz.
         return (ArmutKg*2.14)+(ElmaKg*3.67)+(DomatesKg*1.11)+(MuzKg*0.95)+(PatlicanKg*5.00);
     }
 }
--- src/PatikaPlus.java
+++ src/PatikaPlus.java
@@ -12,19 +12,28 @@ public class PatikaPlus {
     }
 
     private static void ManavKasaProgrami() {
+        //ManavKasaProgrami object olusturuldu
         ManavKasaProgrami manavKasaProgrami = new ManavKasaProgrami();
+        //Scanner object olusturuldu
         Scanner input = new Scanner(System.in);
+        //Kullanıcıdan alınan degerler set edildi
         System.out.println("Mana Kasa Programı");
         System.out.println("Armut Kaç Kilo ? : ");
+        //setArmutKg methodu ile armutKg degiskenine deger atandı
         manavKasaProgrami.setArmutKg(input.nextDouble());
         System.out.println("Elma Kaç Kilo ? : ");
+        //setElmaKg methodu ile elmaKg degiskenine deger atandı
         manavKasaProgrami.setElmaKg(input.nextDouble());
         System.out.println("Domates Kaç Kilo ? : ");
+        //setDomatesKg methodu ile domatesKg degiskenine deger atandı
         manavKasaProgrami.setDomatesKg(input.nextDouble());
         System.out.println("Muz Kaç Kilo ? : ");
+        //setMuzKg methodu ile muzKg degiskenine deger atandı
         manavKasaProgrami.setMuzKg(input.nextDouble());
         System.out.println("Patlıcan Kaç Kilo ? : ");
+        //setPatlicanKg methodu ile patlicanKg degiskenine deger atandı
         manavKasaProgrami.setPatlicanKg(input.nextDouble());
+        //hesapla methodu ile toplam tutar hesaplandı
         System.out.println("Toplam Tutar : " + manavKasaProgrami.hesapla());
     }
 
