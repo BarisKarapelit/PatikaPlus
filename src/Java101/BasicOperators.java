cb812900859eded8c61c373d35c4ae8d63084954
baris.karapelit <baris.karapelit@viennalife.com.tr>
2023-09-16 14:36:11 +0300

    Java101 Course

---
+++ src/Java101/BasicOperators.java
@@ -0,0 +1,9 @@
+package Java101;
+
+public class BasicOperators {
+    public static void main(String[] args){
+        int a = 5, b= 2, c = 3, d = 4, e = 5, f = 6, g = 7, h = 8;
+        boolean result= a == b;
+        System.out.printf("a == b : %b\n", result);
+    }
+}
