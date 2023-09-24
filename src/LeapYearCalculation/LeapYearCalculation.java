5b8a7e6355af38a827222b8233acd52f1a4c0bba
baris.karapelit <baris.karapelit@viennalife.com.tr>
2023-09-25 01:01:54 +0300

    FlightTicketPriceCalculation

---
+++ src/LeapYearCalculation/LeapYearCalculation.java
@@ -0,0 +1,29 @@
+package LeapYearCalculation;
+
+import java.util.Scanner;
+
+public class LeapYearCalculation {
+    private static Integer year;
+
+    public static void setYear(Integer year) {
+        LeapYearCalculation.year = year;
+    }
+    public static Integer getYear() {
+        return year;
+    }
+    public static boolean isLeapYear() {
+        System.out.println("Please enter a year:");
+        Scanner scanner = new Scanner(System.in);
+        setYear(scanner.nextInt());
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                System.out.println("Year " + year + " is a leap year.");
+                return year % 400 == 0;
+            }
+            System.out.println("Year " + year + " is a leap year.");
+            return true;
+        }
+        System.out.println("Year " + year + " is not a leap year.");
+        return false;
+    }
+}
