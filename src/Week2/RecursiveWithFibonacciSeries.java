62f4aba09b92e34bc7cd342c2f90400d5740b649
baris.karapelit <baris.karapelit@viennalife.com.tr>
2023-09-27 01:04:09 +0300

    PalindromNumber

--- src/Week2/RecursiveWithFibonacciSeries.java
+++ src/Week2/RecursiveWithFibonacciSeries.java
@@ -11,6 +11,9 @@ public class RecursiveWithFibonacciSeries {
         for (int i = 0; i < number; i++) {
             System.out.print(fibonacci(i) + " ");
         }
+
+        System.out.println();
+        System.out.println("Sum of " + number + " numbers: " + fibonacci(number));
     }
     static int fibonacci(int n) {
         if (n <= 1) {
