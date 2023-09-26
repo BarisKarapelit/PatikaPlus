d481de71a7c8cc7b6af5c7bdb69776b270bb76f0
baris.karapelit <baris.karapelit@viennalife.com.tr>
2023-09-26 23:25:58 +0300

    Added Weeks

--- src/week3/Main.java
+++ src/week3/Main.java
@@ -1,4 +1,8 @@
 package week3;
 
 public class Main {
+    public static void main(String[] args) {
+        MineSweeper mineSweeper = new MineSweeper(3,3);
+        mineSweeper.run();
+    }
 }
--- src/week3/MineSweeper.java
+++ src/week3/MineSweeper.java
@@ -1,5 +1,68 @@
 package week3;
 
+import java.util.Scanner;
+
 public class MineSweeper {
 
+    private static int rowNumber;
+    private static int columnNumber;
+    public MineSweeper(int rowNumber, int columnNumber) {
+        setRowNumber(rowNumber);
+        setColumnNumber(columnNumber);
+    }
+
+    public void run() {
+        DynamicCentering dynamicCentering = new DynamicCentering();
+        dynamicCentering.setCenteringPage("Welcome to Mine Sweeper!");
+        Boolean isPlay = true, gameStatus = true;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            dynamicCentering.setCenteringPage("Enter the number of rows: ");
+            dynamicCentering.setCenteringPage("Enter the number of columns: ");
+            dynamicCentering.setCenteringPage("====================================");
+            int a= scanner.nextInt();
+            if (a == 1) {
+                gameStatus = true;
+            } else {
+                gameStatus = false;
+            }
+            if (gameStatus) {
+                dynamicCentering.setCenteringPage("You win!");
+                dynamicCentering.setCenteringPage("Do you want to play? (1: Yes, 2: No)");
+                int b = scanner.nextInt();
+                if (b == 1) {
+                    isPlay = true;
+                } else {
+                    isPlay = false;
+                }
+
+            } else {
+                dynamicCentering.setCenteringPage("You lose!");
+                dynamicCentering.setCenteringPage("Do you want to play? (1: Yes, 2: No)");
+                int b = scanner.nextInt();
+                if (b == 1) {
+                    isPlay = true;
+                } else {
+                    isPlay = false;
+                }
+            }
+        }while (isPlay);
+
+    }
+
+    public static void setColumnNumber(int columnNumber) {
+        MineSweeper.columnNumber = columnNumber;
+    }
+
+    public static void setRowNumber(int rowNumber) {
+        MineSweeper.rowNumber = rowNumber;
+    }
+
+    public static int getColumnNumber() {
+        return columnNumber;
+    }
+
+    public static int getRowNumber() {
+        return rowNumber;
+    }
 }
