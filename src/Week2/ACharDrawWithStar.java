59094a06c475198985ee1982ef52016191aeb603
baris.karapelit <baris.karapelit@viennalife.com.tr>
2023-10-09 01:28:09 +0300

    ACharDrawWithStar

---
+++ src/Week2/ACharDrawWithStar.java
@@ -0,0 +1,41 @@
+package Week2;
+
+import java.lang.reflect.Array;
+
+public class ACharDrawWithStar {
+    public static void main(String[] args) {
+        Integer[][] a = new Integer[6][4];
+
+        // Initialize the inner arrays
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                a[i][j] = 0;  // Initialize all elements to 0
+            }
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                if (i == 0 || i == 2) {
+                    a[i][j] = 1;
+                } else if (j == 0 || j == 3) {
+                    a[i][j] = 1;
+                }else if (i == 5 && j == 2) {
+                    a[i][j] = 1;
+                }else if (i == 5 && j == 1) {
+                    a[i][j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                if (a[i][j] == 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();  // Move this inside the outer loop
+        }
+    }
+}
