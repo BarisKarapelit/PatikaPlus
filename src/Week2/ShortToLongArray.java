8a2d6022893c9d0e180ce2fc74dc1dc46e2ac5be
baris.karapelit <baris.karapelit@viennalife.com.tr>
2023-10-09 01:45:28 +0300

    ShortToLongArray

--- src/Week2/ShortToLongArray.java
+++ src/Week2/ShortToLongArray.java
@@ -25,5 +25,19 @@ public class ShortToLongArray {
         for (int i = 0; i < size; i++) {
             System.out.print(shortArray[i] + " ");
         }
+        System.out.println();
+        int[] count= new int[size];
+        for (int i = 0; i < size; i++) {
+            for (int j = i+1; j < size-1; j++) {
+                if (shortArray[i] == shortArray[j]) {
+                    count[i]++;
+                }
+            }
+        }
+        for (int i=0;i<size;i++){
+            if (count[i]>0){
+                System.out.println("Tekrar sayısı: "+count[i]+ "Tekrar eden sayı: "+shortArray[i]);
+            }
+        }
     }
 }
