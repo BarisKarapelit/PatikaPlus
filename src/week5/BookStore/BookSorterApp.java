c52f84e5dbfc60dadb94a834c6bc2ebd9b73640d
baris.karapelit <baris.karapelit@viennalife.com.tr>
2023-10-22 01:40:29 +0300

    BookStore

--- src/week5/BookStore/BookSorterApp.java
+++ src/week5/BookStore/BookSorterApp.java
@@ -6,6 +6,6 @@ public class BookSorterApp implements Comparator<Book> {
 
     @Override
     public int compare(Book o1, Book o2) {
-        return o2.getBookName().compareTo(o1.getBookName());
+        return o1.getPages()-o2.getPages();
     }
 }
---
+++ src/week5/Fixture/Fixture.java
@@ -0,0 +1,5 @@
+package week5.Fixture;
+
+public class Fixture {
+
+}
