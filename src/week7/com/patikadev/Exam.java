bcdd2f44004c8419f3b747d6d04fb26fe265d241
baris.karapelit <baris.karapelit@viennalife.com.tr>
2023-10-24 20:57:27 +0300

    week-7

---
+++ src/week7/com/patikadev/Exam.java
@@ -0,0 +1,53 @@
+package week7.com.patikadev;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Exam extends JFrame {
+    private JPanel wrapper;
+    private JPanel JPanelTop;
+    private JPanel JPanelBottom;
+    private JTextField pasword;
+    private JButton girisButton;
+    private JTextField user;
+    private JTextField password;
+
+
+    public Exam() {
+        for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+            if ("Mimbus".equals(info.getName())) {
+                try {
+                    System.out.println(info.getName() + "=>" + info.getClassName());
+                    UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+
+            }
+        }
+        add(wrapper);
+        setSize(500, 500);
+        int x = (Toolkit.getDefaultToolkit().getScreenSize().width - getSize().width) / 2;
+        int y = (Toolkit.getDefaultToolkit().getScreenSize().height - getSize().height) / 2;
+        setLocation(x, y);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setTitle("Sınav");
+        setVisible(true);
+        setResizable(false);
+        girisButton.addActionListener(e -> {
+            String username = user.getText();
+            String password = Exam.this.password.getText();
+            if (username.equals("") || password.equals("")) {
+                JOptionPane.showMessageDialog(Exam.this, "Kullanıcı adı veya şifre boş olamaz");
+                return;
+            }
+            if (username.equals("admin") && password.equals("12345")) {
+                JOptionPane.showMessageDialog(Exam.this, "Giriş Başarılı");
+            } else {
+                JOptionPane.showMessageDialog(Exam.this, "Giriş Başarısız");
+            }
+        });
+    }
+}
---
+++ src/week7/com/patikadev/Main.java
@@ -0,0 +1,7 @@
+package week7.com.patikadev;
+
+public class Main {
+    public static void main(String[] args) {
+        Exam exam = new Exam();
+    }
+}
